a07b80101c50bbac2ba7d17be73abc88a628a7a0
leoboyerbx <contact@leoboyer.fr>
2021-02-02 11:24:14 +0100

    before back to imac

--- Swift-Lvl-2 Sans correction.playground/Pages/Fonctions.xcplaygroundpage/Contents.swift
+++ Swift-Lvl-2 Sans correction.playground/Pages/Fonctions.xcplaygroundpage/Contents.swift
@@ -53,4 +53,35 @@ func addMul(firstAdder: Int, secondAdder: Int, multiplier: Int = 1) -> Int
 addMul(firstAdder: 1, secondAdder: 2)
 
 
+// Exercices de fonctions
+
+// Créer une function sans paramètre avec une valeur de retour de tye tableau de Int avec 3 éléments
+func myFunc () -> [Int]
+{
+    return [4, 6, 90]
+}
+
+myFunc()
+
+// Créer une function avec 3 paramètres Int float et String, Retourne string contenant les 3 éléments
+func concatVars (superInt: Int, superFloat: Float, superString: String) -> String
+{
+    return "\(superInt) \(superFloat) \(superString)"
+}
+
+concatVars(superInt: 56, superFloat: 49.76, superString: "Coucou ;)")
+
+// Créer une fonction avec 3 paramètres dont un par défaut. Si la valeur par défaut est modifiée, afficher "spécifique", sinon afficher "défaut"
+
+func defaultFunc (param1: Int, param2: Int, param3: Float = 43.2)
+{
+    if param3 == 43.2 {
+        print("Default")
+    } else {
+        print("Spécifique")
+    }
+}
+
+defaultFunc(param1: 34, param2: 12)
+defaultFunc(param1: 34, param2: 12, param3: 45.4)
 //: [Next](@next)
